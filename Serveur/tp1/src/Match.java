4188cd4832686f1b031f7aa8c94d17a97a1abec5
Philippe-Antoine Plante <phil2396@gmail.com>
2015-09-04 14:13:09 -0400

    done

--- Serveur/tp1/src/Match.java
+++ Serveur/tp1/src/Match.java
@@ -1,22 +1,19 @@
-import java.util.List;
-
-
 public class Match {
 
 	private Equipe[] equipes  = new Equipe[2];
 	private int[] pointage =  new int[2];
 	private int[] buts =  new int[2];
 	private int[] penalites =  new int[2];
-	private Chronometre chronometre;
+	private Chronometre chronometre= new Chronometre();
 	
 	public Equipe[] getEquipes() {
 		return equipes;
 	}
-	public void setEquipe1(Equipe a) {
-		equipes[0] =a;
+	public void setEquipe1(Equipe e1) {
+		equipes[0] =e1;
 	}
-	public void setEquipe2(Equipe a) {
-		equipes[1] =a;
+	public void setEquipe2(Equipe e2) {
+		equipes[1] =e2;
 	}
 	public void setEquipes(Equipe[] equipes) {
 		this.equipes = equipes;
@@ -30,11 +27,25 @@ public class Match {
 	public int[] getButs() {
 		return buts;
 	}
+	public int getButsEquipe1() {
+		return buts[0];
+	}
+	public int getPenalitesEquipe1() {
+		return penalites[0];
+	}
+	public int getPenalitesEquipe2() {
+		return penalites[1];
+	}
 	public int[] getPenalites() {
 		return penalites;
 	}
 	public Chronometre getChronometre() {
 		return chronometre;
+	}
+	public Match(Equipe e1, Equipe e2) {
+		equipes[0] =e1;
+		equipes[1] =e2;
+		chronometre.demarer();
 	}
 
 	
