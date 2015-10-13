d0fca19c492a321bf44b5dc9ed005c09ee211033
Philippe-Antoine Plante <phil2396@gmail.com>
2015-10-13 18:22:43 -0400

    gestion des joueurs de l'équipe

--- HockeyLiveServer/src/models/Team.java
+++ HockeyLiveServer/src/models/Team.java
@@ -1,8 +1,16 @@
 package models;
+
+import java.util.ArrayList;
+
 public class Team {
 	private String name;
+	private static ArrayList<Player> teamPlayers;
 
 	public Team(String name) {
+		this.teamPlayers = new ArrayList<Player>();
 		this.name = name;
+	}
+	public void addPlayer(Player p) {
+		this.teamPlayers.add(p);
 	}
 }
